dc13b1c088bee59d2dc129225f3b836b4f08edad
bbdeveloper2018 <bbdeveloper2018@gmail.com>
2018-12-12 15:37:38 -0800

    refactored static file paths to clips dir

--- ClipKeeper.Server.WebService/Startup.cs
+++ ClipKeeper.Server.WebService/Startup.cs
@@ -13,11 +13,14 @@ using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ClipKeeper.Server.Data;
 using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace ClipKeeper.Server.WebService
 {
     public class Startup
     {
+        private readonly IHostingEnvironment _env;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,13 +34,14 @@ namespace ClipKeeper.Server.WebService
             services.AddDbContext<ClipKeeperContext>(
                 options => options.UseSqlite(
                     Configuration.GetConnectionString("ClipKeeperDbConnection")));
-
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var clipsPath = Path.Combine(env.ContentRootPath, "clips");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -54,23 +58,22 @@ namespace ClipKeeper.Server.WebService
                 context.Database.EnsureCreated();
             }
 
+            
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions
             {
-                FileProvider = new PhysicalFileProvider(
-                   "C:\\Users\\jhdl\\dev\\vue\\bluebird\\server\\public"),
+                FileProvider = new PhysicalFileProvider(clipsPath),
                 RequestPath = "/static"
             });
 
             app.UseDirectoryBrowser(new DirectoryBrowserOptions
             {
-                FileProvider = new PhysicalFileProvider(
-                    "C:\\Users\\jhdl\\dev\\vue\\bluebird\\server\\public"),
+                FileProvider = new PhysicalFileProvider(clipsPath),
                 RequestPath = "/static"
             });
 
             app.UseHttpsRedirection();
             app.UseMvc();
-        }
+        }        
     }
 }
